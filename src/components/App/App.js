9f2f7eaee9f748b7f59cfe0017002e4762aa4d6d
DeNaje Ferguson <denaje.ferguson@gmail.com>
2022-04-21 10:25:27 -0600

    Convert App to a class component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,28 @@
+import React from 'react';
 import { Header } from '../Header/Header.js';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-    </div>
-  );
+import { fetchArtworks } from '../../apiCalls.js';
+
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      
+    }
+  }
+
+
+
+  render() {
+    return (
+      <div className="App">
+        <Header />
+      </div>
+    );
+  }
+
 }
 
 export default App;
