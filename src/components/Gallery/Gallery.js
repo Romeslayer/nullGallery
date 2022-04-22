670105a1dbf79b885105c9efe5bebce6082a91b5
DeNaje Ferguson <denaje.ferguson@gmail.com>
2022-04-22 11:24:09 -0600

    Create carousel for images

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -1,4 +1,6 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import './Gallery.css'
+import {FaArrowAltCircleRight, FaArrowAltCircleLeft} from 'react-icons/fa'
 
 export class Gallery extends Component {
   constructor(props){
@@ -7,18 +9,38 @@ export class Gallery extends Component {
     this.state = {
       images: this.props.gallery,
       current: 0,
+      length: this.props.gallery.length
     }
   }
+
+  nextSlide = () => {
+    this.state.current === this.state.length - 1 ? this.setState({current: 0}) : this.setState({current: this.state.current + 1 });
+  }
+
+  prevSlide = () => {
+    this.state.current === 0 ? this.setState({current: this.state.length - 1}) : this.setState({current: this.state.current - 1});
+  }
+
     render() {
-      let displayedImages = this.state.images.map(art => {
-        return <img src={art.image} alt='art' />
-      })
+
+
+      let displayedImage = (!Array.isArray(this.state.images) || this.state.images.length <= 0) ? null : this.state.images.map((art, index) => {
+          return(
+            <div className={index === this.state.current ? 'slide active' : 'slide'} key={art.id} >
+              {index === this.state.current && <img className='carousel-image' src={art.image} alt={art.title}  />}
+            </div>
+
+          )
+        })
+
         return (
           <>
             <h1>Welcome to your gallery</h1>
-            <div>
-                {displayedImages}
-            </div>
+            <section className="carousel">
+              <FaArrowAltCircleLeft className='icon left-arrow' tabIndex='1' onClick={() => this.prevSlide()} />
+              <FaArrowAltCircleRight className='icon right-arrow' tabIndex='0' onClick={() => this.nextSlide()} />
+                {displayedImage}
+            </section>
           </>
         )
     }
