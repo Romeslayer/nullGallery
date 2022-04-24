d79f33fe2e501f57d1dc0038eac6bfed8d4801ef
DeNaje Ferguson <denaje.ferguson@gmail.com>
2022-04-23 22:22:04 -0600

    Create home button and accompanying logic

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,10 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 import './Header.css'
 import headerBackground from "../../assets/header-bg.png"
 export const Header = (props) => {
+
+  let homeButton = (props.home ? '' : <Link to="/home"><button className="home-button">home</button></Link>)
 
     return (
         <header>
@@ -9,6 +12,7 @@ export const Header = (props) => {
           <img src={headerBackground} />
         </div>
         <h1 className="title">null Gallery</h1>
+        {homeButton}
         </header>
     )
 }
