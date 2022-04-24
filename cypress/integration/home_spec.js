84ec86f9d356f31475e4d98d5e08bde9e60d8d4e
DeNaje Ferguson <denaje.ferguson@gmail.com>
2022-04-24 15:34:03 -0600

    Create tests for home page

--- cypress/integration/home_spec.js
+++ cypress/integration/home_spec.js
@@ -3,21 +3,60 @@ import { fetchArtworksResponse, fetchArtDetailsResponse } from '../fixtures/resp
 describe('null Gallery home page', () => {
 
   beforeEach(() => {
-    cy.visit('localhost:3000/')
-    cy.intercept('GET', `https://api.artic.edu/api/v1/artworks/search?q=abstract?query[term][image_id][must_not]=null`,fetchArtworksResponse)
+    cy.intercept('GET', `https://api.artic.edu/api/v1/artworks/search?q=abstract?query[term][image_id][must_not]=null`, fetchArtworksResponse)
       .as('fetchArtworks');
 
-    cy.intercept('GET', 'https://api.artic.edu/api/v1/artworks?ids=70374,70125,77615',fetchArtDetailsResponse)
+    cy.intercept('GET', 'https://api.artic.edu/api/v1/artworks?ids=70374,70125,77615', fetchArtDetailsResponse)
       .as('fetchArtDetails');
+
+      cy.visit('localhost:3000/');
   })
 
   it('url should be redirected from / to /home', () => {
-    cy.url().should('eql', 'http://localhost:3000/home')
+    cy.url()
+      .should('eql', 'http://localhost:3000/home');
   })
 
   it('should load with the title null Gallery', () => {
-    cy.get('header').contains('null Gallery');
+    cy.get('header')
+      .contains('null Gallery');
   });
 
-  it('should load with three images')
+  it('should load with three images', () => {
+    cy.get('.card-display')
+      .children()
+        .should('have.length', 3)
+    cy.get('[alt="Painting (Figures with Stars)"]')
+    cy.get('[alt="The Key"]')
+    cy.get('[alt="Visions of Eternity"]')
+
+
+  })
+
+  it('should be able to add a image to the gallery by clicking an empty heart', () => {
+    cy.get('.card-button')
+      .first()
+        .click();
+  })
+
+  it('should be able to add a different image to the gallery by clicking an two hearts', () => {
+    cy.get('.card-button')
+      .first()
+        .click();
+    cy.get('.card-button').last()
+        .click();
+  })
+
+  it('after adding images to the gallery the footer should appear with the saved images and button to the gallery', () => {
+    cy.get('.card-button')
+      .first()
+        .click()
+      cy.get('.card-button').last()
+        .click();
+      cy.get('.mini-img-display')
+        .children()
+          .should('have.length', 2);
+      cy.get('.mini-img[alt="Painting (Figures with Stars)"]')
+      cy.get('.mini-img[alt="Visions of Eternity"]')
+  })
 })
