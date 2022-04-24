8321140e4c62d9f0a9c91319f27c0a88677fa58e
DeNaje Ferguson <denaje.ferguson@gmail.com>
2022-04-23 20:55:44 -0600

    Style Header component and add image

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import './Header.css'
+import headerBackground from "../../assets/header-bg.png"
 export const Header = (props) => {
 
     return (
         <header>
-        <h1>null Gallery</h1>
+        <div className="header-background">
+          <img src={headerBackground} />
+        </div>
+        <h1 className="title">null Gallery</h1>
         </header>
     )
 }
