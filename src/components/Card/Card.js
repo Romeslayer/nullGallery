6f678c6499acfce7e6005f3958d6cac196d5d499
DeNaje Ferguson <denaje.ferguson@gmail.com>
2022-04-24 17:42:03 -0600

    Add Proptypes

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './Card.css';
 
 export function Card({ art, toggle }) {
@@ -10,4 +11,9 @@ export function Card({ art, toggle }) {
           {button}
         </div>
     )
+}
+
+Card.propType = {
+  art: PropTypes.object.isRequired,
+  toogle: PropTypes.func.isRequired
 }
--- src/components/MainDisplay/MainDisplay.js
+++ src/components/MainDisplay/MainDisplay.js
@@ -14,6 +14,7 @@ export function MainDisplay({artworks, removeArtwork, saveArtwork}){
         </section>
     )
 }
+
 MainDisplay.propTypes = {
   artworks: PropTypes.array.isRequired,
   removeArtwork: PropTypes.func,
